5587daabf2c4328c69e03c96edce0e4649716e34
MEDO <meddooo2@gmail.com>
2015-02-05 20:44:14 +0100

    promjena indeksa i page2

--- index.php
+++ index.php
@@ -3,6 +3,32 @@
 	<title>git</title>
 </head>
 <body>
-	<h1>Index fajl</h1>
+	<h1>Index fajlll promjena</h1>
+	<p>prvo je potrebno da kazes da si korisnik i uneses ime i email</p>
+
+	<h1>komande</h1>
+	<ul>
+		<li><a href="#">git config --global user.name "medo"</a></li>
+		<li><a href="#">git config --global user.email medd@com</a></li>
+		<li><a href="#">git init</a></li>
+		<li><a href="#">git status</a></li>
+		<li><a href="#">git log</a></li>
+		<li><h3>Dodavanje duagacko</h3></li>
+		
+		<li><a href="#">git add --all</a></li>
+		<li><a href="#">git commit  </a> otvara se vim upises poruku pa :qw</li>
+
+		<li><h3>Skaraćeno dodavanje</h3></li> 
+		<li><a href="#">git commit  -a -m "adsadsad" </a></li>
+
+	</ul>
+
+
+	<h1>bash komande korisno</h1>
+	<ul>
+		<li>ls -la</li>
+	</ul>
+
+	<div id="commit">Moze da se odradi na više nacina</div>	
 </body>
 </html>
