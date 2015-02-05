9da62c4da290727f3ac6fa22349f8d3b4a073361
MEDO <meddooo2@gmail.com>
2015-02-05 21:27:42 +0100

    test 2 posle comit

---
+++ crazy.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>git</title>
+</head>
+<body>
+	<h1>crazy</h1>
+</body>
+</html>
---
+++ page2.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>git</title>
+</head>
+<body>
+	<h1>page2</h1>
+</body>
+</html>
