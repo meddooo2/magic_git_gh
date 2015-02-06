24fb4c26615cc45ba1e768dfadfc3b4619b5539d
MEDO <meddooo2@gmail.com>
2015-02-06 03:31:38 +0100

    prije distributed

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
  		<li><a href="#">git  revert "id.."   # napravice novi comit a checkout nece</a></li>
 		<li><a href="#">git  checkout --index.php  # varati verziju index fajla iz head</a></li>
 		<li><a href="#">git  checkout id... index.php  # varati verziju zavisno od id</a></li>
+		<li><a href="#">git  checkout id...   # varati verziju svih fajlova na posebnoj grani</a></li>
 		<li><a href="#">git  reset --hard</a></li>
 		<li><a href="#">git reflog # prikazuje sve sta sam radjo ne samo comite</a></li>
 		<li><h3>bisect -binarna pretraga za greskom</h3></li>
@@ -40,7 +41,8 @@
 		<li><a href="#">git bisect bad HEAD # </a></li>
 		<li><a href="#">git bisect start # </a></li>
 		<li><a href="#">git bisect bad # ako nisi naso bug</a></li>
-
+		<li><a href="#">git bisect good # ako si naso posle mozes da sačuvas kao novu granu pa da eventualno spojis sa master</a></li>
+		<li><a href="#">git bisect reset # brise bisect granu i vraca te na master</a></li>
 	</ul>
 
 
